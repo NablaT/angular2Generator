65f20439e002cd69e129a261dd15858056b08363
NablaT <remi.pourtier@gmail.com>
2016-03-10 10:52:32 +0100

    filling component template

--- generator-angular2project/routeur/templates/components/_basic-template.ts
+++ generator-angular2project/routeur/templates/components/_basic-template.ts
@@ -1,3 +1,12 @@
 /**
  * Created by rpourtier on 10/03/2016.
  */
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'app',
+    moduleId: module.id,
+    templateUrl: './app.component.html',
+    styleUrls : ['./app.component.css']
+})
+export class AppComponent { }
