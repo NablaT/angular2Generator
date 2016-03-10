b6fee97131ed62bbbc23ccbe88a3fb160adcda9f
NablaT <remi.pourtier@gmail.com>
2016-03-10 10:50:06 +0100

    Component generation script works

--- generator-angular2project/app/index.js
+++ generator-angular2project/app/index.js
@@ -42,5 +42,6 @@ module.exports = generators.Base.extend({
 
     install: function(){
         this.npmInstall();
+        //this.spawnCommand('dir');
     }
 });
--- generator-angular2project/routeur/index.js
+++ generator-angular2project/routeur/index.js
@@ -1,14 +1,33 @@
 var generators = require('yeoman-generator');
+var lodash = require('lodash');
 
-module.exports = generators.Base.extend({prompting: function () {
-    var done = this.async();
-    this.prompt({
-        type    : 'input',
-        name    : 'name',
-        message : 'Your project name',
-        default : this.appname // Default to current folder name
-    }, function (answers) {
-        this.log(answers.name);
-        done();
-    }.bind(this));
-}});
+module.exports = generators.Base.extend({
+
+    constructor:function(){
+        generators.Base.apply(this, arguments);
+        this.arguments = lodash.camelCase(this.arguments);
+        this.log(this.arguments);
+    },
+    /*
+    prompting: function () {
+        var done = this.async();
+        this.prompt({
+            type: 'input',
+            name: 'name',
+            message: 'Your project name',
+            default: this.appname // Default to current folder name
+        }, function (answers) {
+            this.log(answers.name);
+            this.log(generators.arguments);
+            done();
+        }.bind(this));
+    },*/
+
+    writing: function () {
+       this.basicTemplate = 'app/components/' + lodash.kebabCase(this.arguments);
+
+        this.copy('components/_basic-template.html', this.arguments+'/'+this.basicTemplate + '.html');
+        this.copy('components/_basic-template.ts', this.arguments+'/'+this.basicTemplate + '.ts');
+    },
+
+});
