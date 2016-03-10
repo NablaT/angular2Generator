4cff903074994ba71ea0de4cef947adbb09cf294
NablaT <remi.pourtier@gmail.com>
2016-03-10 12:24:20 +0100

    fixing component structure bugs

--- generator-angular2project/component/templates/components/_basic-template-test.ts
+++ generator-angular2project/component/templates/components/_basic-template-test.ts
@@ -1,3 +1,11 @@
 /**
  * Created by rpourtier on 10/03/2016.
  */
+import {<%= nameOfComponent %>} from "./<%= argsInKebab %>.component";
+import {Component} from "angular2/core";
+@Component({
+    selector: 'test-cmp',
+    template: '<sd-<%= argsInKebab %>></sd-<%= argsInKebab %>>',
+    directives: [<%= nameOfComponent %>]
+})
+class Test<%= nameOfComponent %> {}
