076b60853a24e3f8f9432852f12e8117739edb63
NablaT <remi.pourtier@gmail.com>
2016-03-11 12:14:04 +0100

    Adding methods to manage styles

--- generator-angular2project/style/index.js
+++ generator-angular2project/style/index.js
@@ -1,3 +1,24 @@
 /**
  * Created by rpourtier on 11/03/2016.
  */
+var generators = require('yeoman-generator');
+var lodash = require('lodash');
+
+module.exports = generators.Base.extend({
+
+    constructor: function () {
+        generators.Base.apply(this, arguments);
+        this.reworkArguments = lodash.camelCase(this.arguments);
+        this.nameOfStyle= this.reworkArguments.charAt(0).toUpperCase() + this.reworkArguments.slice(1);
+    },
+
+    writing: function () {
+        this.argsInKebab = lodash.kebabCase(this.arguments);
+
+        this.basicTemplateSrc = 'app/shared/styles/' + this.argsInKebab;
+
+        this.copy('styles/_basic-template.css', this.basicTemplateSrc + '.css');
+        this.copy('styles/_basic-template.scss', this.basicTemplateSrc + '.scss');
+    },
+
+});
