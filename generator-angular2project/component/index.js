ed0e5be4beed8c13de74f8968f99c89a4e5bffd3
NablaT <remi.pourtier@gmail.com>
2016-03-16 17:26:18 +0100

    Cleaning index,js component and fixing bugs

--- generator-angular2project/component/index.js
+++ generator-angular2project/component/index.js
@@ -1,52 +1,31 @@
 var generators = require('yeoman-generator');
 var lodash = require('lodash');
-var CommonFunctions = require('../common-functions.js');
 var json = require('fs');
 
 module.exports = generators.Base.extend({
 
+    /**
+     * Generator constructor. It reworks the arguments.
+     */
     constructor: function () {
         generators.Base.apply(this, arguments);
         this.reworkArguments = lodash.camelCase(this.arguments);
         this.nameOfComponent= this.reworkArguments.charAt(0).toUpperCase() + this.reworkArguments.slice(1);
-        this.log(this.nameOfComponent);
-        this.log(this.arguments);
-        //CommonFunctions.readJson();
     },
 
-
     /**
-     * Function readJson. This function reads the file package.json to know if user asked to install sass.
+     * Function checkSass. This function reads the file package.json to know if user asked to install sass.
      */
-    readJson: function () {
-        this.log("in readJson");
+    checkSass: function () {
         this.hasSass=false;
-        this.hasBootstrap=false;
-        this.hasFoundation=false;
-        this.hasFontAwesome=false;
         var jsonContent=json.readFileSync("./package.json", 'utf8');
         var storeJson= JSON.parse(jsonContent);
-        this.log(storeJson);
         for(var currentKey in storeJson.dependencies) {
             if(currentKey=="gulp-sass"){
                 this.hasSass=true;
             }
-            if(currentKey==="bootstrap"){
-                this.hasBootstrap=true;
-            }
-            else if(currentKey=="foundation"){
-                this.hasFoundation=true;
-            }
-            if(currentKey=="font-awesome"){
-                this.hasFontAwesome=true;
-            }
-            this.log("key:"+currentKey+", value:"+storeJson.dependencies[currentKey]);
         }
-
-        this.log("bootstrap: "+this.hasBootstrap+" foundation: "+this.hasFoundation+" sass: "+this.hasSass+" fontawesome: "+this.hasFontAwesome);
-
     },
-    
 
     /**
      * Function writing. This function copies the basic templates for components.
@@ -56,7 +35,12 @@ module.exports = generators.Base.extend({
         this.basicTemplate = 'app/components/' + this.argsInKebab + '/' + this.argsInKebab;
         this.copy('components/_basic-template.html', this.basicTemplate + '.component.html');
         this.copy('components/_basic-template.ts', this.basicTemplate + '.component.ts');
-        this.copy('components/_basic-template.css', this.basicTemplate + '.component.css');
+        if(this.hasSass){
+            this.copy('components/_basic-template.css', this.basicTemplate + '.component.scss');
+        }
+        else{
+            this.copy('components/_basic-template.css', this.basicTemplate + '.component.css');
+        }
         this.copy('components/_basic-template-test.ts', this.basicTemplate + '.component.spec.ts');
     },
 
